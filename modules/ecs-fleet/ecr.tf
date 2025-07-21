2134d3ce92526edb6e231efeba374159e93e12e7
goncalofonseca <goncalofnsc@gmail.com>
2025-07-21 14:21:29 +0100

    structure

--- modules/ecs-fleet/ecr.tf
+++ modules/ecs-fleet/ecr.tf
@@ -1,9 +1,15 @@
 locals {
-  runner_image = var.runner_image != "" ? var.runner_image : "${aws_ecr_repository.runner.repository_url}:${var.runner_image_tag}"
+  runner_image = var.runner_image != "" ? var.runner_image :
+    "${aws_ecr_repository.runner.repository_url}:${var.runner_image_tag}"
   label_images = {
     for label, _ in var.extra_runner_images : label =>
     "${aws_ecr_repository.runner.repository_url}:${var.runner_image_tag}-${label}"
   }
+  build_images = merge({
+    base = "${path.root}/runner"
+  }, {
+    for label, dir in var.extra_runner_images : label => "${path.root}/${dir}"
+  })
 }
 
 resource "aws_ecr_repository" "runner" {
@@ -11,39 +17,25 @@ resource "aws_ecr_repository" "runner" {
 }
 
 resource "null_resource" "build_runner_image" {
+  for_each = local.build_images
+
   triggers = {
-    # Rebuild if the Dockerfile changes or a new tag is provided
-    dockerfile_sha = filesha1("${path.root}/runner/Dockerfile")
-    image_tag      = var.runner_image_tag
+    dockerfile_sha = filesha1("${each.value}/Dockerfile")
+    image_tag = var.runner_image_tag
   }
 
   provisioner "local-exec" {
-    command     = <<EOT
-    aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${aws_ecr_repository.runner.repository_url}
-    docker build --platform linux/amd64 -t ${aws_ecr_repository.runner.repository_url}:${var.runner_image_tag} ..
-    docker push ${aws_ecr_repository.runner.repository_url}:${var.runner_image_tag}
-    EOT
+    command = each.key == "base" ? join("\n", [
+      "aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${aws_ecr_repository.runner.repository_url}",
+      "docker build --platform linux/amd64 -t ${aws_ecr_repository.runner.repository_url}:${var.runner_image_tag} ${each.value}",
+      "docker push ${aws_ecr_repository.runner.repository_url}:${var.runner_image_tag}"
+    ]) : join("\n", [
+      "aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${aws_ecr_repository.runner.repository_url}",
+      "docker build --platform linux/amd64 --build-arg BASE_IMAGE=${local.runner_image} -t ${aws_ecr_repository.runner.repository_url}:${var.runner_image_tag}-${each.key} ${each.value}",
+      "docker push ${aws_ecr_repository.runner.repository_url}:${var.runner_image_tag}-${each.key}"
+    ])
     interpreter = ["bash", "-c"]
   }
-}
 
-resource "null_resource" "build_extra_images" {
-  for_each = var.extra_runner_images
-
-  triggers = {
-    dockerfile_sha = filesha1("${path.module}/../${each.value}/Dockerfile")
-    image_tag      = var.runner_image_tag
-  }
-
-  provisioner "local-exec" {
-    command     = <<EOT
-    aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${aws_ecr_repository.runner.repository_url}
-    docker build --platform linux/amd64 \
-      --build-arg BASE_IMAGE=${local.runner_image} \
-      -t ${aws_ecr_repository.runner.repository_url}:${var.runner_image_tag}-${each.key} ../${each.value}
-    docker push ${aws_ecr_repository.runner.repository_url}:${var.runner_image_tag}-${each.key}
-    EOT
-    interpreter = ["bash", "-c"]
-  }
-  depends_on = [null_resource.build_runner_image]
+  depends_on = each.key == "base" ? [] : [null_resource.build_runner_image["base"]]
 }
